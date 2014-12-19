6029ad72c0e43a0065e317d87e13fa97f45e32a8
Prakhar Srivastav <prakhar1989@gmail.com>
2014-12-19 23:43:02 +0300

    First commit on new repo

--- release.sh
+++ release.sh
@@ -1,5 +1,10 @@
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 hugo --theme=skeleton
 git add -A
-git commit
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
 git push origin master
 git subtree push --prefix=public git@github.com:prakhar1989/prakhar1989.github.com.git gh-pages
