12d8336e1cfb95c40de5f0910a2cb6ffeff9bdd6
Prakhar Srivastav <prakhar1989@gmail.com>
2014-12-19 23:59:30 +0300

    Test commit

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 hugo --theme=skeleton
+cd public
 git add -A
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
@@ -7,4 +8,3 @@ if [ $# -eq 1 ]
 fi
 git commit -m "$msg"
 git push origin master
-git subtree push --prefix=public git@github.com:prakhar1989/prakhar1989.github.com.git gh-pages
