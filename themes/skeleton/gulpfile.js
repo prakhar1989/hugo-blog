3d719b477d8bf46459ec231600920a6d23b6bf16
Prakhar Srivastav <prakhar1989@gmail.com>
2015-01-22 22:12:51 +0300

    Added CSS minification with gulp

---
+++ themes/skeleton/gulpfile.js
@@ -0,0 +1,22 @@
+var gulp      = require('gulp'),
+    concat    = require('gulp-concat'),
+    rename    = require('gulp-rename'),
+    minifyCSS = require('gulp-minify-css');
+
+
+var fileOrder = [
+  'static/css/ionicons.min.css', 
+  'static/css/normalize.css', 
+  'static/css/skeleton.css', 
+  'static/css/main.css', 
+  'static/css/railcasts.css'
+];
+
+gulp.task('default', function() {
+  gulp.src(fileOrder)
+      .pipe(concat('styles.css'))
+      .pipe(gulp.dest('static'))
+      .pipe(minifyCSS())
+      .pipe(rename('styles.min.css'))
+      .pipe(gulp.dest('static'))
+});
