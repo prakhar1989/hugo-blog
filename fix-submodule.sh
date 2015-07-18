026bc3ecf1ced8173c4ae7db29b2fa54874e8728
Prakhar Srivastav <prakhar1989@gmail.com>
2015-07-19 00:14:42 +0300

    Added a simple script to help dealing with submodule errors

--- fix-submodule.sh
+++ fix-submodule.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+# Use this script to deal with submodule errors
+# with Git. Generally avoid committing files when the 
+# Go server is running.
+
 rm .gitmodules
 rm -rf .git/modules
-echo "Remove entry for submodule from git config file. Opening file in 2secs..."
+echo -e "\033[0;32mRemove entry for submodule from git config file. Opening file in 2secs..\033[0m"
 sleep 2
 vim .git/config
 rm -rf public
 git add . && git commit -m "cleaning index"
 git submodule add git@github.com:prakhar1989/prakhar1989.github.com.git public
 ./release.sh
+git add .gitmodule
+git commit -m "Added the submodule back"
