d3027c59695c1598a85260ce6301f50ed29b6349
Prakhar Srivastav <prakhar1989@gmail.com>
2015-07-19 00:06:09 +0300

    merged js files into one. Added a gulp build step for JS

--- themes/skeleton/gulpfile.js
+++ themes/skeleton/gulpfile.js
@@ -1,23 +1,37 @@
 var gulp      = require('gulp'),
     concat    = require('gulp-concat'),
     rename    = require('gulp-rename'),
-    minifyCSS = require('gulp-minify-css');
+    minifyCSS = require('gulp-minify-css'),
+    uglify    = require('gulp-uglify');
 
 
 var fileOrder = [
-  'static/css/ionicons.min.css', 
-  'static/css/normalize.css', 
-  'static/css/skeleton.css', 
-  'static/css/main.css', 
-  'static/css/highlight.css',
-  'static/css/zoom.css'
+    'static/css/ionicons.min.css', 
+    'static/css/normalize.css', 
+    'static/css/skeleton.css', 
+    'static/css/main.css', 
+    'static/css/highlight.css',
+    'static/css/zoom.css'
+];
+
+var jsFileOrder = [
+    'static/js/transition.js',
+    'static/js/zoom.min.js'
 ];
 
 gulp.task('default', function() {
-  gulp.src(fileOrder)
-      .pipe(concat('styles.css'))
-      .pipe(gulp.dest('static'))
-      .pipe(minifyCSS())
-      .pipe(rename('styles.min.css'))
-      .pipe(gulp.dest('static'))
+    // concat and minify css
+    gulp.src(fileOrder)
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('static'))
+        .pipe(minifyCSS())
+        .pipe(rename('styles.min.css'))
+        .pipe(gulp.dest('static'));
+
+    // concat and minify js
+    gulp.src(jsFileOrder)
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('static'));
 });
+
