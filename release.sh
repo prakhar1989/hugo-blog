00a0f41f25a9d3d6c591dddba447e018ecf92346
Prakhar Srivastav <prakhar1989@gmail.com>
2017-02-16 15:16:09 -0800

    moved to Firebase!

--- release.sh
+++ release.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 hugo --theme=skeleton
-cd public
-git add -A
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-git push origin master
+firebase deploy
